584a9283cf6362cef9f67d9ef5753b263f8157ba
youhyunwoo99 <casshyunwoo@naver.com>
2023-05-26 20:58:31 +0900

    5/26 Personal Information Modification

--- src/db-config.js
+++ src/db-config.js
@@ -1,10 +1,10 @@
-//const mysql = require('mysql');
+
 const dbConfig ={
-    host:'127.0.0.1',
-    port: '3306',
-    user:'root',
-    password:'1234',
-    database:'IoT'
+    host:'',
+    port: '',
+    user:'',
+    password:'',
+    database:''
 };
 
 module.exports = dbConfig;
